5a9646cef8735c2583fa0a47b51a50b6f398af68
Michael Rolli <michael@rollis.ch>
2023-10-18 05:36:39 +0200

    feat: bump azurerm provider version to latest

--- main.tf
+++ main.tf
@@ -4,16 +4,21 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.69.0"
+      version = "=3.76.0"
     }
   }
 }
 
 # Configure the Microsoft Azure Provider
+#
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
 provider "azurerm" {
   features {}
 }
 
+# Create a resource group
+#
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group
 resource "azurerm_resource_group" "tf-testgroup" {
   name     = "unibe-idsys-dev-tf-testgroup"
   location = "switzerlandnorth"
