9d6919e0cfca3eda8c9fda1c4e0588ba39d5cdf3
Michael Rolli <michael@rollis.ch>
2023-10-18 07:48:11 +0200

    feat(network): add a virtual network to the setup

--- main.tf
+++ main.tf
@@ -22,6 +22,24 @@ provider "azurerm" {
 resource "azurerm_resource_group" "tf-testgroup" {
   name     = "unibe-idsys-dev-tf-testgroup"
   location = "switzerlandnorth"
+
+  tags = {
+    environment = "dev"
+    division    = "id"
+    subDivision = "sys"
+  }
+}
+
+# Create a virtual network
+# For more flexibility, we do not define subnets inline but as separate resources.
+#
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/virtual_network
+resource "azurerm_virtual_network" "tf-testnetwork" {
+  name                = "unibe-idsys-dev-tf-testnetwork"
+  resource_group_name = azurerm_resource_group.tf-testgroup.name
+  location            = azurerm_resource_group.tf-testgroup.location
+  address_space       = ["10.123.0.0/16"]
+
   tags = {
     environment = "dev"
     division    = "id"
