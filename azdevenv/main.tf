f043b72be6c4ccadf1418a01a6c2c542658ac6b9
Michael Rolli <michael@rollis.ch>
2023-10-28 05:37:25 +0200

    refactor: hardcode tenant_id and subscription_id
    Should these two values be set once there as hardcoded value as these
    are fixed for this project? These are more like constants (per project)
    and not variables in the very sense of its name.

--- azdevenv/main.tf
+++ azdevenv/main.tf
@@ -3,8 +3,8 @@
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
 provider "azurerm" {
   features {}
-  tenant_id                  = var.tenant_id
-  subscription_id            = var.subscription_id
+  tenant_id                  = "d400387a-212f-43ea-ac7f-77aa12d7977e" # == UniBE
+  subscription_id            = "9671b6ad-4877-4a42-9609-9eaf88283097" # == UniBE - IDSYS - DEV - 021-14
   skip_provider_registration = true
 }
 
--- azdevenv/variables.tf
+++ azdevenv/variables.tf
@@ -1,13 +1,3 @@
-variable "tenant_id" {
-  type    = string
-  default = "d400387a-212f-43ea-ac7f-77aa12d7977e" # == UniBE
-}
-
-variable "subscription_id" {
-  type    = string
-  default = "9671b6ad-4877-4a42-9609-9eaf88283097" # == UniBE - IDSYS - DEV - 021-14
-}
-
 variable "host_os" {
   type = string
 }
