248130ba2e1f4020aac467810d8c0e2f651c3f20
Michael Rolli <michael@rollis.ch>
2024-06-12 09:31:13 +0200

    feat: add snappass contianer using cli
    This script runs snappass as a container app building a container based
    on the source and uses a add-on Redis as the backend.

---
+++ snappass_cli/containerapp_dev_redis_addon.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+# The following commands are used to install the Azure CLI and the latest Container Apps extension
+#
+# az upgrade
+# az extension add --name containerapp --upgrade --allow-preview true
+# az provider register --namespace Microsoft.App
+# az provider register --namespace Microsoft.OperationalInsights
+
+if [ ! -d snappass ]; then
+  git clone https://github.com/id-unibe-ch/snappass
+  git -C snappass checkout 369-envvar-redis-password
+fi
+
+service="snappass-cli"
+environment="dev"
+resource_group="rg-$service-$environment"
+location="switzerlandnorth"
+log_analytics_workspace="log-$service-$environment"
+ca_name="ca-$service-$environment"
+ca_environment="cae-$service-$environment"
+# container add-on binding complains about - and _
+ca_addon_redis="caa${service//-/}redis${environment}"
+
+az group create \
+  --name $resource_group \
+  --location $location
+
+az monitor log-analytics workspace create \
+  --resource-group $resource_group \
+  --workspace-name $log_analytics_workspace \
+  --location $location
+
+workspace_customerid=$(az monitor log-analytics workspace show \
+  --resource-group $resource_group \
+  --workspace-name $log_analytics_workspace \
+  --query customerId \
+  --output tsv)
+
+workspace_key=$(az monitor log-analytics workspace get-shared-keys \
+  --resource-group $resource_group \
+  --workspace-name $log_analytics_workspace \
+  --query primarySharedKey \
+  --output tsv)
+
+az containerapp env create \
+  --name $ca_environment \
+  --resource-group $resource_group \
+  --logs-workspace-id "$workspace_customerid" \
+  --logs-workspace-key "$workspace_key" \
+  --location $location
+
+az containerapp add-on redis create \
+  --resource-group $resource_group \
+  --name $ca_addon_redis \
+  --environment $ca_environment
+
+az containerapp up \
+  --name $ca_name \
+  --resource-group $resource_group \
+  --environment $ca_environment \
+  --source="./snappass/" \
+  --location $location \
+  --browse
+
+az containerapp update \
+  --name $ca_name \
+  --bind $ca_addon_redis \
+  --resource-group $resource_group
